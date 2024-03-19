b1e9ae88154afd22da30b424580012b0c1646075
mwigoti <henrymwoha02@gmail.com>
2024-03-19 09:17:44 -0500

    l

--- base/views.py
+++ base/views.py
@@ -13,8 +13,21 @@ def doLogin(request):
     if request.method != "POST":
         return HttpResponse("<h2> Method Not Allowed </h2>")
     else:
-        user=EmailBackEnd.authenticate(request,request.POST.get("email"), request.POST.get("password"))
+        user=EmailBackEnd.authenticate(request,username=request.POST.get("email"), password=request.POST.get("password"))
         if user != None:
+            login(request.user)
             return HttpResponse("email :"+request.POST.get("email")+"Password: "+request.POST.get("password"))
         else:
             return HttpResponse("Invalid Login")
+
+def GetUserDetails(request):
+    if request.user != None:
+
+        return HttpResponse("User :" +request.user.email+"user_type :"+request.user.user_type)
+
+    else:
+        return HttpResponse("Please Login First")
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")
