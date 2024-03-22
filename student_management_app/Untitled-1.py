b8c81c68a118162d2630b713ba0397c14f10f09d
mwigoti <104128222+mwigoti@users.noreply.github.com>
2024-03-22 19:03:03 +0300

    Update Untitled-1.py
    l

--- student_management_app/Untitled-1.py
+++ student_management_app/Untitled-1.py
@@ -8,6 +8,6 @@ eq3= Eq(a - 42.41*b + 1798.6081*e, -42.696)
 eq4=Eq(a - 48.47*b + 93.89*c - 4550.8483*d+2349.3409*e, -15.742)
 eq5=Eq(a + 36.35*b + 87.84*c + 3192.984*d + 1321.3225*e, -8.85)
 
-solution = solve((eq1,eq2,eq3,eq4,eq5), (a,b,c,d,e))
+sol = solve((eq1,eq2,eq3,eq4,eq5), (a,b,c,d,e))
 
-print(solution)
+print(sol)
