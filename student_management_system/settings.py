ec9ab36c1c7ba2d033943edb8f56c97691247f20
mwigoti <henrymwoha02@gmail.com>
2024-05-02 02:10:24 -0500

    REQ

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -28,13 +28,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
 STATIC_URL="/static/"
-STATIC_ROOT=os.path.join(BASE_DIR,"static")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Application definition
 
