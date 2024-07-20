5b6a658e3053bfef5b0f9303bab79da45102f96c
mwigoti <henrymwoha02@gmail.com>
2024-07-20 15:51:33 +0300

    database

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -1,7 +1,6 @@
 from django.utils import timezone
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth import get_user_model
-import datetime
 
 from django.contrib import messages
 from django.contrib.auth import  login, logout
